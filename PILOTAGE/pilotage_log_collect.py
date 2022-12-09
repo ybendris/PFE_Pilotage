d18dba6706cda0080804b7a800c6ce8450a7b7c5
Valek16 <59090064+Valek16@users.noreply.github.com>
2022-12-09 09:15:39 +0100

    Récupération nom de session pour DATA et LOG COLLECT à l'aide d'un msg commande

--- PILOTAGE/pilotage_log_collect.py
+++ PILOTAGE/pilotage_log_collect.py
@@ -25,24 +25,37 @@ header = ['date/time', 'level', 'msg']
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
 
 class LogCollector(NetworkItem):
-    def __init__(self, host, port, name, abonnement, session, dt_string):
+    def __init__(self, host, port, name, abonnement, dt_string):
 
         #print(f"{host},{port},{name},{abonnement}")
 
         #Nom de la session
-        self.session = session
+        self.session = ""
         #Date et heure de lancement du log_collect
         self.dt_string = dt_string
+
+        # clé = expéditeur_paquet et valeur = DictWriter
+        self.writers = {}
+
+        # clé = expéditeur et valeur = fichier ouvert
+        self.fOpens = {}
 
         try:
             # On crée le répertoire CSV_LOG dans le répertoire courant, s'il n'existe pas
             os.mkdir("CSV_LOG")
         except FileExistsError:
             pass
+        """
         #On crée et ouvre le fichier CSV, où les logs sont stockés
         #Le fichier est nommé selon le nom de la session et de la date et l'heure de lancement du log_collect
-        self.fOpen = self.openCSV('CSV_LOG/LOG_{}_{}.csv'.format(self.session, self.dt_string), 'w')
+        self.fOpen = self.openCSV('CSV_LOG/LOG_{}_{}.csv'.format(self.session, self.dt_string), 'w')"""
         NetworkItem.__init__(self, host, port, name, abonnement)
+
+
+    def recupererNomSession(self,message):
+        print(message["msg"]["session"])
+        self.session=message["msg"]["session"]
+        print("NOM DE LA SESSION : " + self.session)
 
 
     """
@@ -57,7 +70,7 @@ class LogCollector(NetworkItem):
     ------
     Un file object
     """
-    def openCSV(self, file, mode):
+    """def openCSV(self, file, mode):
         #si le mode d'ouverture est 'écriture'
         if mode == 'w':
             #ouvrir le fichier
@@ -70,7 +83,7 @@ class LogCollector(NetworkItem):
             self.writer.writeheader()
             return fopen
         else:
-            pass
+            pass"""
 
     """
     Écrire dans un fichier CSV
@@ -79,10 +92,55 @@ class LogCollector(NetworkItem):
     ----------
     message : le message reçu contenant le log à enregistrer 
     """
-    def ecrireCSV(self, message):
+    """def ecrireCSV(self, message):
         logging.info(f"On écrit: {message} sur {self.writer}")
         #Écrire le message dans le fichier
-        self.writer.writerow(message)
+        self.writer.writerow(message)"""
+
+    def ecrireCSV(self, message):
+        """origine = message["expediteur"]
+        paquet = message["paquet"]
+        separateur = "_"
+        """
+        cle = self.session
+        print(cle)
+
+        #si le fichier correspondant à l'entité expéditrice est déjà ouvert
+        if cle in self.writers:
+            #print("existe")
+            #print(self.fOpens[cle])
+            #print(self.writers[cle])
+            logging.info(f"On écrit: {message}")
+
+            #Écrit la donnée dans le fichier CSV
+            self.writers[cle].writerow(message)
+
+        #Si le fichier n'existe pas
+        else:
+            print("existe pas")
+
+            #Crée un header basé sur les clés du message
+            header = list(message.keys())
+            # header=["MESSAGE"]
+            #print(type(header))
+
+            #Crée et ouvre un fichier correspondant au nom de la session, à l'entité expéditrice de données
+            #et à la date et heure
+            self.fOpens[cle] = open('CSV_LOG/LOG_{}_{}.csv'
+                                        .format(self.session, self.dt_string), 'w', newline='')
+
+            #print(self.fOpens[cle])
+            #Crée le file writer object correspondant au fichier de l'entité
+            self.writers[cle] = csv.DictWriter(self.fOpens[cle], fieldnames=header)
+
+            #print(self.writers)
+            #Écrit le header sur la 1er ligne du fichier
+            self.writers[cle].writeheader()
+            logging.info(f"On écrit: {message}")
+
+            #Écrit la donnée dans le fichier CSV
+            self.writers[cle].writerow(message)
+            #print(header)
 
     """
     Processus principal du log_collect
@@ -90,13 +148,23 @@ class LogCollector(NetworkItem):
     """
     def service(self):
         print("service")
+        flag_session = False
         while True:
             #Récupère un message dans la queue
             deserialized_message = self.queue_message_to_process.get()
             # print(f"deserialized_message: {deserialized_message}")
 
+            if flag_session == False:
+                if deserialized_message["type"]=="CMD":
+                    if "session" in deserialized_message["msg"]:
+                        print("on recupére le nom de la session")
+                        self.recupererNomSession(deserialized_message)
+                        flag_session = True
+                    else :
+                        pass
+
             #si le message est un log
-            if deserialized_message["type"] == 'LOG':
+            if deserialized_message["type"] == 'LOG' and flag_session == True:
 
                 #Retirer le type du message
                 del deserialized_message["type"]
@@ -108,11 +176,11 @@ class LogCollector(NetworkItem):
 
 if __name__ == '__main__':
     logging.info('starting')
-    if len(sys.argv) != 3:
+    """if len(sys.argv) != 3:
         print(f"Usage: {sys.argv[0]} <name> <session>")
-        sys.exit(1)
-    name = sys.argv[1]
-    SESSION_NAME = sys.argv[2]
+        sys.exit(1)"""
+    name = "LOG_COLLECT" #sys.argv[1]
+    #SESSION_NAME = sys.argv[2]
     dt_string = getBeginDateTime()
     abonnement = ["LOG"]
-    logCollect = LogCollector(HOST, PORT, name, abonnement, SESSION_NAME , dt_string)
+    logCollect = LogCollector(HOST, PORT, name, abonnement, dt_string)
