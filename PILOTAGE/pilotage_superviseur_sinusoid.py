32c8c0730f293f15fcd87f2f83e3d3156cd55ddd
ybendris <yannis.bendris@ig2i.centralelille.fr>
2023-01-05 18:14:36 +0100

    Documentation Superviseur Sinusoïde

--- PILOTAGE/pilotage_superviseur_sinusoid.py
+++ PILOTAGE/pilotage_superviseur_sinusoid.py
@@ -35,6 +35,16 @@ class SuperviseurSinus(NetworkItem):
         self.go()
 
     def go(self):
+        """
+        Initialise les paramètres de données et de génération de données aléatoires
+        avec des valeurs aléatoires.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+
+        Returns:
+            None
+        """
         self.data_params = ['{}.sin'.format(self.name)]
         self.data = {'{}.sin'.format(self.name):deque()}
 
@@ -45,14 +55,36 @@ class SuperviseurSinus(NetworkItem):
         self.d3 = random.random()*2*np.pi
         self.pause_until = 0
         
+
     def gen_param(self):
+        """
+        Génère des paramètres aléatoires pour la génération de données aléatoires.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+
+        Returns:
+            None
+        """
         self.fq=random.randint(1,6)/4
         self.a0=random.randint(0,20)/10
         self.a1=random.randint(5,10)
         self.a2=random.randint(0,20)/random.randint(3,8)
         self.a3=random.randint(0,20)/random.randint(3,5)
 		
+
     def action_regen_param(self, *karg):
+        """
+        Régénère les paramètres de génération de données aléatoires
+        en tenant compte de la dernière valeur générée et en utilisant des valeurs aléatoires.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            *karg (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
         print(karg)
         fq=random.randint(1,6)/4
         a0=random.randint(0,20)/10*10
@@ -72,8 +104,19 @@ class SuperviseurSinus(NetworkItem):
             self.yfix = ytransit
             self.pause_until = self.last+delay
             #self.action_pause(delay)
-            
+
+
     def next_y(self, y_target):
+        """
+        Calcule l'instant auquel la génération de données atteindra la valeur cible y_target.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            y_target (float): Valeur cible à atteindre.
+
+        Returns:
+            float: Instant auquel la génération de données atteindra la valeur cible.
+        """
         x = np.linspace(self.last, self.last+2, 2000)
         y = self.a0+self.a1*np.sin(2*np.pi*self.fq*x+self.d1)+self.a2*np.sin(2*2*np.pi*self.fq*x+self.d2)+self.a3*np.sin(3*2*np.pi*self.fq*x+self.d3)
         calc = np.around(np.absolute(y-y_target),1)
@@ -83,23 +126,81 @@ class SuperviseurSinus(NetworkItem):
         
         return i/1000		
 
+
     def action_accelerate(self, *karg):
+        """
+        Accélère la génération de données.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            *karg (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
         self.action_accelerate_to(1.2)
 
+
     def action_decelerate(self, *karg):
+        """
+        Ralentit la génération de données.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            *karg (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
         self.action_accelerate_to(0.6)
 
+
     def action_accelerate_to(self, val, *karg):
+        """
+        Accélère la génération de données à la vitesse spécifiée.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            val (float): Vitesse à laquelle accélérer la génération de données.
+            *karg (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
+
         x = self.last
         self.d1 -= 2*np.pi*x*self.fq*(val-1)
         self.d2 -= 2*2*np.pi*x*self.fq*(val-1)
         self.d3 -= 3*2*np.pi*x*self.fq*(val-1)
         self.fq *= val
 
+
     def action_pause_1(self, *param):
+        """
+        Met en pause la génération de données pendant 1 unité de temps.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            *param (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
         self.action_pause(1, *param)
 		
+
     def action_pause(self, delay, *param):
+        """
+        Met en pause la génération de données pendant un certain délai.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            delay (float): Durée de la pause.
+            *param (tuple): Tuples de valeurs supplémentaires passées à la fonction.
+
+        Returns:
+            None
+        """
         x = self.last
         if self.pause_until < self.last:
             self.pause_until = self.last+delay
@@ -119,13 +220,17 @@ class SuperviseurSinus(NetworkItem):
         self.d2 -= 2*2*np.pi*self.fq*delay
         self.d3 -= 3*2*np.pi*self.fq*delay
 
-    """
-    Remplie une structure de donnée (self.data) avec des données formant la somme de 3 sinusoïdes
-
-
-
-    """
+    
     def remplit(self):
+        """
+        Remplit les données de l'objet avec des valeurs générées aléatoirement ou en pause.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+
+        Returns:
+            None
+        """
         if 'last' not in self.__dict__ or self.last is None:
             self.first = round(time.perf_counter(),3)
             self.last = self.first
@@ -136,9 +241,7 @@ class SuperviseurSinus(NetworkItem):
                 now = self.pause_until
             val0 = int((self.last-self.first)*1000)
             nbval = int((now-self.last)*1000)
-
             #y = np.full(nbval, self.yfix) (Code LA)
-
             self.data['{}.sin'.format(self.name)].append({'date':self.last+self.delta, 'counter':val0, 'data':{"time":time.perf_counter(),"test_sinus":self.yfix }})
             self.last = now
             logging.info("cas 2")
@@ -160,16 +263,45 @@ class SuperviseurSinus(NetworkItem):
             self.data['{}.sin'.format(self.name)].append({'date':self.last+self.delta, 'counter':val0, 'data':{"time":time.perf_counter(),"test_sinus":y2 }})
             self.last = now
 
+
     def traiterData(self, data):
-        pass
+        """
+        Traite les données reçues.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            data (dict): Les données à traiter.
+
+        Returns:
+            None
+        """
+        logging.info(f"Le {self.name} ne traite pas les messages de type DATA")
+
 
     def traiterLog(self, log):
-        pass
+        """
+        Traite les log reçues.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+            log (dict): Les log à traiter.
+
+        Returns:
+            None
+        """
+        logging.info(f"Le {self.name} ne traite pas les messages de type LOG")
     
-    """
-    Fonction définissant les actions de la classe
-    """
+
     def define_action(self):
+        """
+        Définit les actions disponibles dans le Superviseur Sinusoïde.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+
+        Returns:
+            list: Une liste d'actions sous la forme de dictionnaires contenant un nom (str) et une fonction (callable).
+        """
         actions = [
             {"nom":"stop","function": self.stop},
             {"nom":"dem_accelerate", "function": self.action_accelerate},
@@ -180,10 +312,17 @@ class SuperviseurSinus(NetworkItem):
         
         return actions
     
-    """
-    Processus principal du Superviseur de test
-    """
+    
     def service(self):
+        """
+        Exécute le service principal de l'objet.
+
+        Parameters:
+            self (object): L'objet sur lequel est appelée la méthode.
+
+        Returns:
+            None
+        """
         logging.info("Service global lancé")
         keypress = kb_func()		
         check_data = time.perf_counter()
@@ -199,8 +338,6 @@ class SuperviseurSinus(NetworkItem):
             #Réception de la part des messages venant du CENTRAL
             self.traiterMessage(self.getMessage())		
                     
-            
-            
             #time.sleep(1)
 
             if time.perf_counter() > check_data+0.005: #toute les 5 ms (200hz)
